36a7ee5d477de0c1a8d282aca1628ed33a62c394
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-04-04 12:26:01 -0300

    starting mcreate

--- src/mthread.c
+++ src/mthread.c
@@ -5,8 +5,27 @@
 #include "../include/mthread.h"
 #define NUM_PRIO_LVLS 3
 
-TCB *apto[NUM_PRIO_LVLS] = {NULL, NULL, NULL};
-TCB *executando = NULL;
-TCB *bloqueado = NULL;
+TCB_t *apto[NUM_PRIO_LVLS] = {NULL, NULL, NULL};
+TCB_t *executando = NULL;
+TCB_t *bloqueado = NULL;
 
+int mcreate(int prio, void (*start)(void*), void *arg)
+{
+	ucontext_t cur_context, new_context;
 
+	getcontext(&cur_context);
+	if (executando == NULL) {
+	/* mcreate é chamado pela primeira vez. Cria um TCB_t para main e
+	 * coloca na fila de apto (ou executando? Logo saberemos) */
+		TCB_t *main;	   
+		main = malloc(sizeof(TCB_t));
+		main->tid = 0;
+		main->prio = 0;
+		main->context = cur_context;
+		main->prev = NULL;
+		main->next = NULL;
+		apto[0] = main;
+	}
+
+	char new_stack[SIGSTKSZ];
+}
