1e9e8216b622411edebb5f9d84c10f16f49bb219
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-04-04 13:37:13 -0300

    main_tcb is global

--- src/mthread.c
+++ src/mthread.c
@@ -8,13 +8,13 @@
 TCB_t *apto[NUM_PRIO_LVLS] = {NULL, NULL, NULL};
 TCB_t *executando = NULL;
 TCB_t *bloqueado = NULL;
+TCB_t *main_tcb = NULL;
 
 int initialize()
 {
 	ucontext_t cur_context;
 
 	getcontext(&cur_context);
-	TCB_t *main_tcb;	   
 	main_tcb = malloc(sizeof(TCB_t));
 	main_tcb->tid = 0;
 	main_tcb->prio = 0; /* prioridade alta */
@@ -30,10 +30,11 @@ int mcreate(int prio, void (*start)(void*), void *arg)
 	ucontext_t *new_context;
 
 	getcontext(new_context);
-	if (executando == NULL) {
+	if (main_tcb == NULL) {
 	/* primeira função de mthread chamada. */
 		initialize();
 	}
 
+	printf("%d\n", new_context->uc_link);
 	char new_stack[SIGSTKSZ];
 }
