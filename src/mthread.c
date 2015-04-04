b5c39fc7a21f485c3bd685a3ff3f24b6b70600cf
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-04-04 13:19:01 -0300

    initialize

--- src/mthread.c
+++ src/mthread.c
@@ -9,22 +9,30 @@ TCB_t *apto[NUM_PRIO_LVLS] = {NULL, NULL, NULL};
 TCB_t *executando = NULL;
 TCB_t *bloqueado = NULL;
 
+int initialize()
+{
+	ucontext_t cur_context;
+
+	getcontext(&cur_context);
+	TCB_t *main_tcb;	   
+	main_tcb = malloc(sizeof(TCB_t));
+	main_tcb->tid = 0;
+	main_tcb->prio = 0; /* prioridade alta */
+	main_tcb->context = cur_context;
+	main_tcb->prev = NULL;
+	main_tcb->next = NULL;
+	executando = main_tcb;
+	return 0;
+}
+
 int mcreate(int prio, void (*start)(void*), void *arg)
 {
 	ucontext_t cur_context, new_context;
 
 	getcontext(&cur_context);
 	if (executando == NULL) {
-	/* mcreate é chamado pela primeira vez. Cria um TCB_t para main e
-	 * colocado no processador (executando) */
-		TCB_t *main_tcb;	   
-		main_tcb = malloc(sizeof(TCB_t));
-		main_tcb->tid = 0;
-		main_tcb->prio = 0;
-		main_tcb->context = cur_context;
-		main_tcb->prev = NULL;
-		main_tcb->next = NULL;
-		executando = main_tcb;
+	/* primeira função de mthread chamada. */
+		initialize();
 	}
 
 	char new_stack[SIGSTKSZ];
