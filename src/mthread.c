3f0fce4c445230f61117ac4b34df904cb00bd636
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-04-04 15:47:45 -0300

    initialize; scheduler

--- src/mthread.c
+++ src/mthread.c
@@ -9,32 +9,73 @@ TCB_t *apto[NUM_PRIO_LVLS] = {NULL, NULL, NULL};
 TCB_t *executando = NULL;
 TCB_t *bloqueado = NULL;
 TCB_t *main_tcb = NULL;
+int tids = 0;
+int scheduler();
+int initialize();
 
+/* Aloca espaço pra TCB da main thread. Associa a função 'scheduler' ao
+ * contexto da 'main_tcb' e a coloca no estado 'executando' */
 int initialize()
 {
-	ucontext_t cur_context;
+	ucontext_t *context;
+	char stack[SIGSTKSZ];
 
-	getcontext(&cur_context);
 	main_tcb = malloc(sizeof(TCB_t));
-	main_tcb->tid = 0;
+	main_tcb->tid = tids++;
 	main_tcb->prio = 0; /* prioridade alta */
-	main_tcb->context = cur_context;
 	main_tcb->prev = NULL;
 	main_tcb->next = NULL;
+
+	getcontext(&(main_tcb->context));
+	context = &(main_tcb->context);
+	context->uc_link = NULL; /* FIXME */
+	context->uc_stack.ss_sp = stack;
+	context->uc_stack.ss_size = sizeof(stack);
+
+	makecontext(context, (void (*)(void)) scheduler, 0);
+
 	executando = main_tcb;
+	return 0;
+}
+
+/* a principal função da biblioteca, responsável por gerenciar as tarefas */
+int scheduler()
+{
+	printf("I'm the scheduler\n");
 	return 0;
 }
 
 int mcreate(int prio, void (*start)(void*), void *arg)
 {
-	ucontext_t *new_context;
+	ucontext_t *context;
+	TCB_t *tcb;
+	char stack[SIGSTKSZ];
 
-	new_context = malloc(sizeof(ucontext_t));
-	getcontext(new_context);
+	if (prio < 0 || prio > NUM_PRIO_LVLS - 1) {
+		printf("error: invalid priority level: %d\n", prio);
+		return -1;
+	}
+
 	if (main_tcb == NULL) {
 	/* primeira função de mthread chamada. */
 		initialize();
 	}
 
-	char new_stack[SIGSTKSZ];
+	/* Cria nova TCB e insere na fila de prioridades */
+	tcb = malloc(sizeof(TCB_t));
+	tcb->tid = tids++;
+	tcb->prio = prio;
+	tcb->prev = NULL;
+	tcb->next = apto[prio];
+	apto[prio] = tcb;
+
+	/* cria novo contexto a partir a partir do atual. */
+	/* context é só pra facilitar o acesso a tcb->context */
+	getcontext(&(tcb->context));
+	context = &(tcb->context);
+	context->uc_link = &(main_tcb->context);
+	context->uc_stack.ss_sp = stack;
+	context->uc_stack.ss_size = sizeof(stack);
+
+	return tcb->tid;
 }
