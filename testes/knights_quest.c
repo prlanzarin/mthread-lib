aee3b4353f6aa75a856f5a0c94a9c9d9d5f98389
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-05-04 17:01:19 -0300

    work on knights quest: added new structure to maintain position; modified moveKnight to allow thread creation with void parameter

--- testes/knights_quest.c
+++ testes/knights_quest.c
@@ -1,92 +1,111 @@
 #include "../include/mthread.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX_BSIZE 8
 #define MIN_BSIZE 1
 
 typedef struct KnightPosition {
 	int c;
 	int r;
-	struct KnightPosition *next;
 } KnightPos;
 
-void insert_move(KnightPos *pos, KnightPos **queue)
+typedef struct KnightPositionList {
+	KnightPos pos;
+	struct KnightPositionList *next;
+} KnightPosEl;
+
+KnightPosEl *next_moves = NULL;
+
+/* lista de posições -> inserção */
+void insert_move(KnightPosEl *pos, KnightPosEl **queue)
 {
 	pos->next = *queue;
 	*queue = pos;
 }
 
-KnightPos *moveKnight(int c, int r)
+/* determina todas as próximas posições que o cavalo pode atingir dada a
+ * posição (c, r) de entrada
+ */
+KnightPosEl *moveKnight(void *arg)
 {
-	int c1, r1, r2;
+	int c1, r1, r2, cpos, rpos;
 	int v, h;
-	KnightPos *next_moves  = NULL, *new_move;
+	KnightPosEl *ptr;
+	KnightPos *pos_ptr;
+	
+	/* angaria os parâmetros passados pela estrutura de posições em arg*/
+	pos_ptr = (KnightPos *)arg;
+	cpos = pos_ptr->c;
+	rpos = pos_ptr->r;
 
+	/* calcula as próximas possíveis posições do cavalo a partir de 
+	cpos e rpos */
 	for(v = 2; v >= -2; v--) {
 		switch(v) {
 			case 2:
-				c1 = c+v;
-				r1 = r-1;
-				r2 = r+1;
+				c1 = cpos+v;
+				r1 = rpos-1;
+				r2 = rpos+1;
 				break;
 			case -2:
-				c1 = c+v;
-				r1 = r-1;
-				r2 = r+1;
+				c1 = cpos+v;
+				r1 = rpos-1;
+				r2 = rpos+1;
 				break;
 			case 1:
-				c1 = c+v;
-				r1 = r-2;
-				r2 = r+2;
+				c1 = cpos+v;
+				r1 = rpos-2;
+				r2 = rpos+2;
+
 				break;
 			case -1:
-				c1 = c+v;
-				r1 = r-2;
-				r2 = r+2;
+				c1 = cpos+v;
+				r1 = rpos-2;
+				r2 = rpos+2;
 				break;
 
 			default: break;
 		}
-
- 		if(c1 <= MAX_BSIZE && c1 >= MIN_BSIZE && v != 0){
-			if(r1 <= MAX_BSIZE && r1 >= MIN_BSIZE) {
-				new_move = malloc(sizeof(KnightPos));
-				new_move->c = c1;
-				new_move->r = r1;
-				insert_move(new_move, &next_moves);
+	/* checa se são posições válidas perante o tabuleiro. Se forem, insere
+	na lista de posições next_moves */
+ 		if(c1 <= 8 && c1 >= 1 && v != 0){
+			if(r1 <= 8 && r1 >= 1) {
+				ptr = malloc(sizeof(KnightPosEl));
+				ptr->pos.c = c1;
+				ptr->pos.r = r1;
+				insert_move(ptr, &next_moves);
 			}
-			if(r2 <= MAX_BSIZE && r2 >= MIN_BSIZE) {
-				new_move = malloc(sizeof(KnightPos));
-				new_move->c = c1;
-				new_move->r = r2;
-				insert_move(new_move, &next_moves);
+			if(r2 <= 8 && r2 >= 1) {
+				ptr = malloc(sizeof(KnightPosEl));
+				ptr->pos.c = c1;
+				ptr->pos.r = r2;
+				insert_move(ptr, &next_moves);
 			}
-			
 		}
 	}
 				
 	return next_moves;
 }
-
-KnightPos *in3(void *arg)
+/*
+bool canReachIn3(KnightPos org, KnightPos dst)
 {
 	int tid;
-	KnightPos *moves_in3, ptr;
-	moves_in3 = moveKnight(c, r);
-	ptr = moves_in3;
+	ptr = next_moves;
 	while(ptr != NULL) {
-		mcreate(2, (void (*)(void *)) in3, (void (*)(void *))
+		tid[i] = mcreate(2, (void *) in3, (void *));
 	}
 }
-
+*/
 int main()
 {
-	int c, r;
-	KnightPos *moves, *ptr;
-	moves = moveKnight(8,1);
+	KnightPosEl *moves, *ptr;
+	KnightPos pos;
+	pos.c = 6; pos.r = 2;
+	moves = moveKnight((void*) &pos);
 	ptr = moves;
 	while(ptr != NULL) {
-		printf("(%d, %d), ", ptr->c, ptr->r);
+		printf("(%d, %d), ", ptr->pos.c, ptr->pos.r);
 		ptr = ptr->next;
 	}
 	printf("\n");
