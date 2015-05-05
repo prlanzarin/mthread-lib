1938a2b938d9d22e6710180b389c524be999175e
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-05-05 16:07:34 -0300

    fixed casts

--- testes/mlock.c
+++ testes/mlock.c
@@ -7,10 +7,10 @@ int tid[MAX_THREADS];
 int counter[50];
 mmutex_t mutex;
 
-void locker_room(void *arg){
+void *locker_room(void *arg){
 	int i = 0;
 	int tindex = *((int *) arg);
-
+	printf("TINDEX %d \n", tindex);
 	printf("Thread %d está executando\n", tid[tindex]);
 	mlock(&mutex);
 
@@ -26,25 +26,28 @@ void locker_room(void *arg){
 	printf("\n");
 	munlock(&mutex);
 	printf("Thread %d saindo da seção crítica\n", tid[tindex]);
+	return;
 }
 
 
 int main() {
 	int i = 0;
 
+	/*if(mlock(&mutex) == 0)
+		printf("A própria main se trancou; isso é possível!\n");
+	munlock(&mutex);*/
+
 	if(mmutex_init(&mutex) == 1){
 		printf("Erro na inicialização do mutex.\n");
 		exit(0);
 }
 	for(i = 0; i < MAX_THREADS; i++) {
-		tid[i] = mcreate(1, (void *) locker_room, (void *) &i);
+		tid[i] = mcreate(1, locker_room, (void *) &i);
 		printf("Thread %d criada\n", tid[i]);
 	}
 
 	for(i = 0; i < MAX_THREADS; i++)
 		mwait(tid[i]);
 
-	if(mlock(&mutex) == 0)
-		printf("A própria main se trancou; isso é possível!\n");
 	exit(0);
 }
