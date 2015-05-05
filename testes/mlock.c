26d06fe8d4e101953d59bfe4efef3479db00636f
prlanzarin <prlanzarin@inf.ufrgs.br>
2015-05-05 17:19:54 -0300

    Fixed problems in mlock, now working as intented. OBS.: theres still a warning regarding void return value. Fixed rc_adder, now working as intended but input gathering is made bit-to-bit. Comments added to tests

--- testes/mlock.c
+++ testes/mlock.c
@@ -1,3 +1,13 @@
+/*
+ * mlock.c: teste da primitiva mlock. Consiste em criar 10 threads que executam
+ * a função locker_room. A função locker_room imprime o TID da thread em questão
+ * 20 vezes. Isso ocorre dentro de uma seção crítica: primeiro há  impressão
+ * de seu TID por 10 vezes e a thread chama myield. Outras threads que tentarem
+ * acessar a seção serão bloqueadas. Após voltar do yield, a thread imprime seu
+ * tid mais 10 vezes.
+ *
+ */
+
 #include "../include/mthread.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,39 +20,43 @@ mmutex_t mutex;
 void *locker_room(void *arg){
 	int i = 0;
 	int tindex = *((int *) arg);
-	printf("TINDEX %d \n", tindex);
-	printf("Thread %d está executando\n", tid[tindex]);
-	mlock(&mutex);
 
-	printf("Thread %d entra na seção crítica e vai imprimir seu tid 10 vezes\n", tid[tindex]);
+	printf("Thread %d está executando\n", tid[tindex]);
+
+	mlock(&mutex);
+	printf("Thread %d ENTRA na seção crítica.\n TID>> ", tid[tindex]);
 	for(i = 0; i<10; i++)
 		printf("%d", tid[tindex]);
 	printf("\n");
-	printf("Thread %d vai usar yield dentro do lock\n", tid[tindex]);
+	printf("Thread %d YIELDED\n", tid[tindex]);
 	myield();
-	printf("Thread %d volta na seção crítica e vai imprimir seu tid 10 vezes\n", tid[tindex]);
+	printf("Thread %d VOLTA a seção crítica após yield.\n TID>> ", tid[tindex]);
 	for(i = 0; i<10; i++)
 		printf("%d", tid[tindex]);
 	printf("\n");
 	munlock(&mutex);
-	printf("Thread %d saindo da seção crítica\n", tid[tindex]);
+	printf("Thread %d SAI da seção crítica\n", tid[tindex]);
+	free(arg);
 	return;
 }
 
 
 int main() {
 	int i = 0;
+	int *index;
 
-	/*if(mlock(&mutex) == 0)
+	if(mlock(&mutex) == 0)
 		printf("A própria main se trancou; isso é possível!\n");
-	munlock(&mutex);*/
+	munlock(&mutex);
 
 	if(mmutex_init(&mutex) == 1){
 		printf("Erro na inicialização do mutex.\n");
 		exit(0);
 }
 	for(i = 0; i < MAX_THREADS; i++) {
-		tid[i] = mcreate(1, locker_room, (void *) &i);
+		index = malloc(sizeof(int));
+		*index = i;
+		tid[i] = mcreate(1, locker_room, (void *) index);
 		printf("Thread %d criada\n", tid[i]);
 	}
 
--- testes/rc_adder.c
+++ testes/rc_adder.c
@@ -1,3 +1,11 @@
+/*
+ * rc_adder.c: teste da primitiva mwait. Consiste na simulação de um 4 bit
+ * ripple carry adder. O usuário deve entrar dois operando, A e B, do bit
+ * mais significativo ao menos significativo. O teste consiste em criar
+ * múltiplas threads, cada uma consistindo de um full adder, em sequência, para
+ * que calculem a soma correspondente e passem o carry out para a thread
+ * seguinte.
+ */ 
 #include "../include/mthread.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,8 +23,8 @@ int tid[MAX_BITS];
 
 FBitOp *full_adder(void *arg){
 	bool ab;
+	/* cálculo da soma e do carry out no full adder */
 	FBitOp *ops = (FBitOp *) arg;
-
 	ab = (ops->a) ^ (ops->b);
 	ops->s = ops->cin ^ ab;
 	ops->cout = (ops->cin & ab) | (ops->a & ops->b);
@@ -27,38 +35,40 @@ FBitOp *full_adder(void *arg){
 
 int main() {
 	int i = 0;
-	size_t bits = 4;
-	bool *a = NULL, *b = NULL;
+	int a, b;
 	FBitOp *sum[MAX_BITS];
-
-	printf("Insira A[3-0]: ");
-	fflush(stdin);
-	getline((char **)&a, &bits, stdin);
-
-	printf("Insira B[3-0]: ");
-	fflush(stdin);
-	getline((char **)&b, &bits, stdin);
-
+	/* solicita ao usuário o primeiro operando A[3-0] */
 	for(i = MAX_BITS-1; i >= 0; i--){
+		printf("Insira A[%d]:", i);
+		scanf("%d", &a);
 		sum[i] = malloc(sizeof(FBitOp));
-		sum[i]->a = (bool) a[i];
-		sum[i]->b = (bool) b[i];
+		sum[i]->a = (bool) a;
+		sum[i]->cin = 0;
 	}
-
+	/* solicita ao usuário o segundo operando B[3-0] */
+	for(i = MAX_BITS-1; i >= 0; i--){
+		printf("Insira B[%d]:", i);
+		scanf("%d", &b);
+		sum[i]->b = (bool) b;
+	}
+	/* criação dos full adders e rapasse dos carry outs para o próximo */
 	for(i = 0; i < MAX_BITS; i++) {
 		tid[i] = mcreate(1, 
 				(void *) full_adder, 
 				(void *) sum[i]);
 		mwait(tid[i]);
+		/* repasse do carry out ao carry in do próximo full adder */
+		if((i + 1) < MAX_BITS)
+			sum[i+1]->cin = sum[i]->cout;
 	}
+	/* impressão do resultado */
 	printf("Resultado: ");
-	for(i = 0; i < MAX_BITS; i++)
+	for(i = MAX_BITS - 1; i >= 0 ; i--)
 		printf("%d", sum[i]->s);
 	printf("\n");
-
+	/*liberação das estruturas de full adder */
 	for(i = MAX_BITS-1; i >= 0; i--)
 		free(sum[i]);
-	free(a);
-	free(b);
+
 	exit(0);
 }
